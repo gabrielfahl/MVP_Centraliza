c0c19144ce7973966af4682a457792b7bd567009
Brafero <brafero22@gmail.com>
2025-01-06 22:06:08 -0300

    Criação da aba de adicionar tarefas

--- flasky.py
+++ flasky.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
 
 # Configuração do Flask e do SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'centralizakey'
 
 
 # Definição dos modelos (Tarefa e Subtarefa)
@@ -35,6 +38,29 @@ def index():
 def visualizar_tarefas():
     tarefas = Tarefa.query.all()
     return render_template('visualizar_tarefas.html', tarefas=tarefas)
+
+@app.route('/adicionar_tarefa', methods=['GET', 'POST'])
+def adicionar_tarefa():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        # Verifica se os campos estão preenchidos
+        if nome and descricao:
+            try:
+                # Tenta criar uma nova tarefa
+                nova_tarefa = Tarefa(nome=nome, descricao=descricao)
+                db.session.add(nova_tarefa)
+                db.session.commit()
+                flash('Tarefa adicionada com sucesso!', 'success')
+            except IntegrityError:
+                # Captura o erro de integridade (nome duplicado)
+                db.session.rollback()  # Reverte qualquer alteração no banco
+                flash('Erro: Nome da tarefa já existe!', 'error')
+            return redirect(url_for('visualizar_tarefas'))
+        else:
+            flash('Por favor, preencha todos os campos.', 'error')
+    return render_template('adicionar_tarefa.html')
+
 
 @app.route("/pomodoro")
 def home():
